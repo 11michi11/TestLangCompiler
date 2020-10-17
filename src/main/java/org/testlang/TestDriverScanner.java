c8025da20784ae7c9e5f339eb5889c98845557a6
alexdima <alexdima@amd-2021>
2020-10-17 23:14:22 +0200

    test2

--- src/main/java/org/testlang/TestDriverScanner.java
+++ src/main/java/org/testlang/TestDriverScanner.java
@@ -1,7 +1,6 @@
 package org.testlang;
 
 
-
 public class TestDriverScanner {
 
     private static final String EXAMPLES_DIR = "src\\main\\resources\\";
@@ -10,7 +9,7 @@ public class TestDriverScanner {
     public static void main(String[] args) {
         SourceFile in = new SourceFile(EXAMPLES_DIR + "loop.tl");
         Scanner s = new Scanner(in);
-
+        
         Token t = s.scan();
         while (t.kind != TokenKind.EOT) {
             System.out.println(t.kind + " " + t.spelling);
